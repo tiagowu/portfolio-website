f727320c5851713973d5cea7b3fad40a31182b68
Tiago <tiagowu12@gmail.com>
2023-06-01 21:48:02 -0400

    Create about section to display a short introduction about myself

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Container } from "@mui/material";
+
+const About = () => {
+  return (
+    <>
+      <Container sx={{ height: "100vh" }} id="about">
+        About
+      </Container>
+    </>
+  );
+};
+
+export default About;
