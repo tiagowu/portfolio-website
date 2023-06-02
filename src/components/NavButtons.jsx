3e13fc885e64262981cd716c8815f631f9c7cfd1
Tiago <tiagowu12@gmail.com>
2023-06-01 21:45:10 -0400

    Create navigation buttons for the navigation bar

---
+++ src/components/NavButtons.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import NavDrawer from "./NavDrawer";
+
+import { Button, IconButton, useMediaQuery, useTheme } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
+const NavButtons = () => {
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const navigations = [
+    { id: 1, label: "About" },
+    { id: 2, label: "Education" },
+    { id: 3, label: "Projects" },
+    { id: 4, label: "Skills" },
+    { id: 5, label: "Contact" },
+  ];
+
+  const [isDrawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setDrawerOpen(!isDrawerOpen);
+  };
+
+  return (
+    <>
+      {isSmallScreen ? (
+        <>
+          <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer}>
+            <MenuIcon />
+          </IconButton>
+          <NavDrawer navigations={navigations} isDrawerOpen={isDrawerOpen} toggleDrawer={toggleDrawer} />
+        </>
+      ) : (
+        navigations.map((item) => (
+          <Button key={item.id} color="inherit">
+            {`${item.id.toString().padStart(2, "0")}. ${item.label}`}
+          </Button>
+        ))
+      )}
+    </>
+  );
+};
+
+export default NavButtons;
