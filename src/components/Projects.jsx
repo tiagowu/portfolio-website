98de643fd4c16d2613d21f7098d503f38894cccf
Tiago <tiagowu12@gmail.com>
2023-06-02 22:35:41 -0400

    Create project section to display personal and academic projects

---
+++ src/components/Projects.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Container } from "@mui/material";
+
+const Projects = () => {
+  return (
+    <>
+      <Container sx={{ height: "100vh", paddingTop: "72px" }} id="projects">
+        Projects
+      </Container>
+    </>
+  );
+};
+
+export default Projects;
