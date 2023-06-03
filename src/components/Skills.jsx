29a3e9cd3619de8960d15e5971f4e5663c7cf253
Tiago <tiagowu12@gmail.com>
2023-06-02 22:36:42 -0400

    Create skills section to display technical skills

---
+++ src/components/Skills.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Container } from "@mui/material";
+
+const Skills = () => {
+  return (
+    <>
+      <Container sx={{ height: "100vh", paddingTop: "72px" }} id="skills">
+        Skills
+      </Container>
+    </>
+  );
+};
+
+export default Skills;
