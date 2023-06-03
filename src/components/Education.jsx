435ffe3fa650a58b4aad596f2008e250e0981271
Tiago <tiagowu12@gmail.com>
2023-06-02 22:24:47 -0400

    Create education section to display college, degree, and relevant courses

---
+++ src/components/Education.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Container } from "@mui/material";
+
+const Education = () => {
+  return (
+    <>
+      <Container sx={{ height: "100vh", paddingTop: "72px" }} id="education">
+        Education
+      </Container>
+    </>
+  );
+};
+
+export default Education;
