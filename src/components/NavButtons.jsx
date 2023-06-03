028ba0ceb7cd64a67e45b43700ebb18bc64a1b64
Tiago <tiagowu12@gmail.com>
2023-06-02 22:29:57 -0400

    Add href to array and handle the navigations when buttons are clicked

--- src/components/NavButtons.jsx
+++ src/components/NavButtons.jsx
@@ -9,17 +9,24 @@ const NavButtons = () => {
   const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
   const navigations = [
-    { id: 1, label: "About" },
-    { id: 2, label: "Education" },
-    { id: 3, label: "Projects" },
-    { id: 4, label: "Skills" },
-    { id: 5, label: "Contact" },
+    { id: 1, label: "About", href: "#about" },
+    { id: 2, label: "Education", href: "#education" },
+    { id: 3, label: "Projects", href: "#projects" },
+    { id: 4, label: "Skills", href: "#skills" },
+    { id: 5, label: "Contact", href: "#contact" },
   ];
 
   const [isDrawerOpen, setDrawerOpen] = useState(false);
 
   const toggleDrawer = () => {
     setDrawerOpen(!isDrawerOpen);
+  };
+
+  const handleNavClick = (href) => {
+    const element = document.querySelector(href);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
   };
 
   return (
@@ -29,11 +36,11 @@ const NavButtons = () => {
           <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer}>
             <MenuIcon />
           </IconButton>
-          <NavDrawer navigations={navigations} isDrawerOpen={isDrawerOpen} toggleDrawer={toggleDrawer} />
+          <NavDrawer navigations={navigations} isDrawerOpen={isDrawerOpen} toggleDrawer={toggleDrawer} handleNavClick={handleNavClick} />
         </>
       ) : (
         navigations.map((item) => (
-          <Button key={item.id} color="inherit">
+          <Button key={item.id} color="inherit" onClick={() => handleNavClick(item.href)}>
             {`${item.id.toString().padStart(2, "0")}. ${item.label}`}
           </Button>
         ))
