3a0d0de3b7a5af55c829a9386bf364bb1c22fdb2
Tiago <tiagowu12@gmail.com>
2023-06-03 21:39:44 -0400

    Add an "About Me" formatted string written using the JavaScript class structure

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,6 +3,17 @@ import React from "react";
 import { Container } from "@mui/material";
 
 const About = () => {
+  const about = `class Tiago {
+    constructor() {
+      this.name = "Tiago Wu";
+      this.current_city = "Staten Island, New York"
+      this.college = "Stony Brook University";
+      this.expected_graduation_date = "May 2024";
+      this.projects = viewProjects();
+      this.skills = viewSkills();
+      this.interests_and_hobbies = [];
+    }\n}`;
+
   return (
     <>
       <Container sx={{ height: "100vh", paddingTop: "72px" }} id="about">
