bc97535c6b6c4cb76ddb025da6f355063a4ac935
Tiago <tiagowu12@gmail.com>
2023-06-04 22:16:27 -0400

    Add table to display course data

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,12 +1,32 @@
 import React from "react";
+import coursesData from "../data/coursesData";
 
-import { Container } from "@mui/material";
+import { Container, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
 
 const Education = () => {
+  const sortedCoursesData = [...coursesData].reverse().sort((a, b) => (a.completed === b.completed ? 0 : a.completed ? 1 : -1));
   return (
     <>
-      <Container sx={{ height: "100vh", paddingTop: "72px" }} id="education">
+      <Container sx={{ paddingTop: "72px" }} id="education">
         Education
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Course Number</TableCell>
+              <TableCell>Course Name</TableCell>
+              <TableCell>Instructor</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {sortedCoursesData.map((course, index) => (
+              <TableRow key={index}>
+                <TableCell>{course.courseNumber}</TableCell>
+                <TableCell>{course.courseName}</TableCell>
+                <TableCell>{course.instructor}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </Container>
     </>
   );
