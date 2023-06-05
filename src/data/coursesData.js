b6254216dd2f050a7d718a6ffb3bf04a8667efb9
Tiago <tiagowu12@gmail.com>
2023-06-04 22:09:32 -0400

    Create course data for education section

---
+++ src/data/coursesData.js
@@ -0,0 +1,76 @@
+const coursesData = [
+  {
+    courseNumber: "CSE 214",
+    courseName: "Data Structures",
+    instructor: "Ahmad Esmaili",
+    completed: true,
+  },
+  {
+    courseNumber: "CSE 215",
+    courseName: "Foundations of Computer Science",
+    instructor: "Christopher Kane",
+    completed: true,
+  },
+  {
+    courseNumber: "CSE 216",
+    courseName: "Programming Abstractions",
+    instructor: "Ritwik Banerjee",
+    completed: true,
+  },
+  {
+    courseNumber: "CSE 220",
+    courseName: "Systems Fundamentals I",
+    instructor: "Joydeep Mitra",
+    completed: true,
+  },
+  {
+    courseNumber: "CSE 303",
+    courseName: "Introduction to the Theory of Computation",
+    instructor: "Anita Wasilewska",
+    completed: true,
+  },
+  {
+    courseNumber: "CSE 304",
+    courseName: "Compiler Design",
+    instructor: "Christopher Kane",
+    completed: false,
+  },
+  {
+    courseNumber: "CSE 310",
+    courseName: "Computer Networks",
+    instructor: "Jalaa Hoblos",
+    completed: false,
+  },
+  {
+    courseNumber: "CSE 312",
+    courseName: "Legal, Social, and Ethical Issues in Information Systems",
+    instructor: "Tony Scarlatos",
+    completed: true,
+  },
+  {
+    courseNumber: "CSE 316",
+    courseName: "Fundamentals of Software Development",
+    instructor: "Christopher Kane",
+    completed: false,
+  },
+  {
+    courseNumber: "CSE 320",
+    courseName: "Systems Fundamentals II",
+    instructor: "Eugene Stark",
+    completed: true,
+  },
+  {
+    courseNumber: "CSE 331",
+    courseName: "Computer Security Fundamentals",
+    instructor: "Omar Chowdhury",
+    completed: false,
+  },
+  {
+    courseNumber: "CSE 373",
+    courseName: "Analysis of Algorithms",
+    instructor: "Rezaul Chowdhury",
+    completed: true,
+  },
+];
+
+export default coursesData;
