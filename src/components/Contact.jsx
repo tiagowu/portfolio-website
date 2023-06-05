506e0d5e59ea4629329cbd1261ab503daf43e8b9
Tiago <tiagowu12@gmail.com>
2023-06-04 22:06:39 -0400

    Create contact section to display personal details and contact form

---
+++ src/components/Contact.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Container } from "@mui/material";
+
+const Contact = () => {
+  return (
+    <>
+      <Container sx={{ height: "100vh", paddingTop: "72px" }} id="contact">
+        Contact
+      </Container>
+    </>
+  );
+};
+
+export default Contact;
