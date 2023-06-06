7d58b5afa70449b1a256c3142dcbc416cc37384b
Tiago <tiagowu12@gmail.com>
2023-06-06 16:28:05 -0400

    Add grid and cards to display projects data

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,14 +1,26 @@
 import React from "react";
+import projectsData from "../data/projectsData";
 
-import { Container } from "@mui/material";
+import { Card, CardContent, Container, Divider, Grid, Typography } from "@mui/material";
 
 const Projects = () => {
   return (
-    <>
-      <Container sx={{ height: "100vh", paddingTop: "72px" }} id="projects">
-        Projects
-      </Container>
-    </>
+    <Container sx={{ paddingTop: "72px" }} id="projects">
+      <Typography variant="h4">Projects</Typography>
+      <Grid container spacing={2} wrap="wrap" justifyContent="center">
+        {projectsData.map((project, index) => (
+          <Grid item key={index} sx={{ alignItems: "center", justifyContent: "center" }}>
+            <Card sx={{ maxWidth: "380px", height: "100%" }}>
+              <CardContent>
+                <Typography variant="h6">{project.projectName}</Typography>
+                <Divider />
+                <Typography variant="body1">{project.description}</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 };
 
