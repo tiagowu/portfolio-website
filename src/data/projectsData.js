eaa96817f7e0e05bfacc05f26a6697209b297360
Tiago <tiagowu12@gmail.com>
2023-06-06 15:39:20 -0400

    Populate project data with personal and academic projects

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,3 +1,56 @@
-const projectsData = [];
+const projectsData = [
+  {
+    projectName: "Lunar System",
+    description: "A menu-based course registration system featuring a serializable database that allows students to manage and view their courses.",
+    completedOn: "Nov 2021",
+    technologies: ["Java"],
+    githubLink: "",
+    demoLink: "",
+  },
+  {
+    projectName: "GeoSym",
+    description:
+      "A symmetry calculator that implements mathematical concepts from group theory to determine the symmetries present in geometric shapes.",
+    completedOn: "Apr 2022",
+    technologies: ["Java"],
+    githubLink: "",
+    demoLink: "",
+  },
+  {
+    projectName: "Temps",
+    description:
+      "A minimalist weather application that enables users to view the weather of their current geolocation as well as any other location in the world.",
+    completedOn: "Aug 2022",
+    technologies: ["HTML/CSS", "JavaScript"],
+    githubLink: "",
+    demoLink: "",
+  },
+  {
+    projectName: "TransforMatrix",
+    description:
+      "A matrix manipulator that extracts data from a txt file, performs transformative operations on the two-dimensional array, and writes the modified contents to an output file.",
+    completedOn: "Aug 2022",
+    technologies: ["MIPS"],
+    githubLink: "",
+    demoLink: "",
+  },
+  {
+    projectName: "MemAlloc",
+    description:
+      "A dynamic memory allocator designed for the x86-64 computer architecture that utilizes a segregated free list and quick list to perform memory management functions such as malloc, free, realloc, and memalign, similar to those in the C programming language.",
+    completedOn: "Mar 2023",
+    technologies: ["C"],
+    githubLink: "",
+    demoLink: "",
+  },
+  {
+    projectName: "Portfolio Website",
+    description: "A portfolio website built to let the world learn more about me through a collection of my experiences, projects, and skills.",
+    completedOn: "In Progress",
+    technologies: ["HTML/CSS", "Javascript"],
+    githubLink: "",
+    demoLink: "",
+  },
+];
 
 export default projectsData;
