fea0a2bc8236d1cce55277b773b10045b9a4856d
Tiago <tiagowu12@gmail.com>
2023-06-05 21:48:20 -0400

    Add preliminary styling to courses table

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,33 +1,79 @@
-import React from "react";
 import coursesData from "../data/coursesData";
 
-import { Container, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+import { Container, Table, TableBody, TableCell, TableHead, TableRow, tableCellClasses } from "@mui/material";
+import { styled } from "@mui/system";
 
 const Education = () => {
+  const StyledEducationContainer = styled(Container)(({ theme }) => ({
+    paddingTop: "72px",
+  }));
+
+  const StyledTableContainer = styled(Container)(({ theme }) => ({
+    overflowX: "auto",
+    paddingLeft: "0px",
+    paddingRight: "0px",
+    "&::-webkit-scrollbar": {
+      height: "4px",
+    },
+    "&::-webkit-scrollbar-thumb": {
+      background: "red",
+      borderRadius: "16px",
+    },
+    "&::-webkit-scrollbar-thumb:hover": {
+      background: "darkgray",
+    },
+  }));
+
+  const StyledTable = styled(Table)(({ theme }) => ({
+    margin: "auto",
+    maxWidth: "600px",
+  }));
+
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: "red",
+      color: "green",
+    },
+    [`&.${tableCellClasses.body}`]: {
+      color: "red",
+    },
+    fontSize: "14px",
+    padding: "12px",
+    whiteSpace: "nowrap",
+    [theme.breakpoints.down("sm")]: {
+      "&:nth-of-type(2)": {
+        whiteSpace: "normal",
+        minWidth: "200px",
+      },
+    },
+  }));
+
   const sortedCoursesData = [...coursesData].reverse().sort((a, b) => (a.completed === b.completed ? 0 : a.completed ? 1 : -1));
   return (
     <>
-      <Container sx={{ paddingTop: "72px" }} id="education">
+      <StyledEducationContainer id="education">
         Education
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Course Number</TableCell>
-              <TableCell>Course Name</TableCell>
-              <TableCell>Instructor</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {sortedCoursesData.map((course, index) => (
-              <TableRow key={index}>
-                <TableCell>{course.courseNumber}</TableCell>
-                <TableCell>{course.courseName}</TableCell>
-                <TableCell>{course.instructor}</TableCell>
+        <StyledTableContainer disableGutters>
+          <StyledTable>
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>Course</StyledTableCell>
+                <StyledTableCell>Course Name</StyledTableCell>
+                <StyledTableCell>Instructor</StyledTableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </Container>
+            </TableHead>
+            <TableBody>
+              {sortedCoursesData.map((course, index) => (
+                <TableRow key={index}>
+                  <StyledTableCell>{course.courseNumber}</StyledTableCell>
+                  <StyledTableCell>{course.courseName}</StyledTableCell>
+                  <StyledTableCell>{course.instructor}</StyledTableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </StyledTable>
+        </StyledTableContainer>
+      </StyledEducationContainer>
     </>
   );
 };
