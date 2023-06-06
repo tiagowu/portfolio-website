95c49a32dbd2c55a662dedeccfb061a94078fbfe
Tiago <tiagowu12@gmail.com>
2023-06-05 21:42:28 -0400

    Adjust breakpoint values in theme

--- src/customTheme.js
+++ src/customTheme.js
@@ -3,9 +3,9 @@ import { createTheme } from "@mui/material";
 const customTheme = createTheme({
   breakpoints: {
     values: {
-      xs: 0,
-      sm: 700,
-      md: 960,
+      xs: 320,
+      sm: 680,
+      md: 900,
       lg: 1280,
       xl: 1920,
     },
