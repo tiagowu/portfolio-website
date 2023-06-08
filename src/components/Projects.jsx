42aa48b0b17f09203b9514e6f938543a91b7bcfd
Tiago <tiagowu12@gmail.com>
2023-06-07 21:45:39 -0400

    Improve layout and styling of each Card

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,20 +1,33 @@
 import React from "react";
 import projectsData from "../data/projectsData";
 
-import { Card, CardContent, Container, Divider, Grid, Typography } from "@mui/material";
+import { Box, Card, CardContent, Container, Divider, Grid, Typography } from "@mui/material";
 
 const Projects = () => {
+  const reversedProjectsData = [...projectsData].reverse();
+
   return (
     <Container sx={{ paddingTop: "72px" }} id="projects">
       <Typography variant="h4">Projects</Typography>
       <Grid container spacing={2} wrap="wrap" justifyContent="center">
-        {projectsData.map((project, index) => (
+        {reversedProjectsData.map((project, index) => (
           <Grid item key={index} sx={{ alignItems: "center", justifyContent: "center" }}>
             <Card sx={{ maxWidth: "380px", height: "100%" }}>
               <CardContent>
-                <Typography variant="h6">{project.projectName}</Typography>
+                <Typography variant="caption" sx={{ margin: "8px" }}>
+                  {project.completedOn}
+                </Typography>
+                <Typography variant="h6" sx={{ textAlign: "center", marginBottom: "12px" }}>
+                  {project.projectName}
+                </Typography>
                 <Divider />
-                <Typography variant="body1">{project.description}</Typography>
+                <Typography variant="caption" sx={{ marginTop: "12px" }}>
+                  {project.description}
+                </Typography>
+
+                <Box>
+                  <Divider orientation="vertical" />
+                </Box>
               </CardContent>
             </Card>
           </Grid>
