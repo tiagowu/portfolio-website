315ac5c6d3d51f87a7995cb8655564598933ce53
Tiago <tiagowu12@gmail.com>
2023-06-07 22:04:15 -0400

    Create footer section to display site navigations and miscellaneous information

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import NavButtons from "./NavButtons";
+
+import { Container } from "@mui/material";
+
+const Footer = () => {
+  return (
+    <Container sx={{ height: "300px", paddingTop: "72px" }} id="footer">
+      Footer
+    </Container>
+  );
+};
+
+export default Footer;
