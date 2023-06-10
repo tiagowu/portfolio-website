4888a8e72879af11b129f729ce131b6a26978293
Tiago <tiagowu12@gmail.com>
2023-06-09 21:23:40 -0400

    Update minor styles and layout  for better visual consistency and responsiveness

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,21 +7,23 @@ const Projects = () => {
   const reversedProjectsData = [...projectsData].reverse();
 
   return (
-    <Container sx={{ paddingTop: "72px" }} id="projects">
-      <Typography variant="h4">Projects</Typography>
-      <Grid container spacing={2} wrap="wrap" justifyContent="center">
+    <Container id="projects" sx={{ pt: "72px" }}>
+      <Typography variant="h4" align="center" paddingBottom="32px">
+        Project
+      </Typography>
+      <Grid container spacing={8} wrap="wrap" justifyContent="center">
         {reversedProjectsData.map((project, index) => (
           <Grid item key={index} sx={{ alignItems: "center", justifyContent: "center" }}>
-            <Card sx={{ maxWidth: "380px", height: "100%" }}>
+            <Card sx={{ maxWidth: "480px", height: "100%" }}>
               <CardContent>
                 <Typography variant="caption" sx={{ margin: "8px" }}>
                   {project.completedOn}
                 </Typography>
-                <Typography variant="h6" sx={{ textAlign: "center", marginBottom: "12px" }}>
+                <Typography variant="h5" sx={{ textAlign: "center", marginBottom: "12px" }}>
                   {project.projectName}
                 </Typography>
                 <Divider />
-                <Typography variant="caption" sx={{ marginTop: "12px" }}>
+                <Typography variant="body1" sx={{ marginTop: "12px" }}>
                   {project.description}
                 </Typography>
                 <Box>
