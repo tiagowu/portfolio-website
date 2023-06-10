4a06ade9090c2ddd455a978910bf0b384185b6a8
Tiago <tiagowu12@gmail.com>
2023-06-09 21:19:14 -0400

    Add short description and update styles and layout for responsive design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,19 @@ import { Box, Container, Typography } from "@mui/material";
 
 const Header = () => {
   return (
-    <Container sx={{ height: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+    <Container maxWidth={false} sx={{ height: "100vh", pt: "72px" }}>
       <NavBar />
-      <Box sx={{ width: 300, textAlign: "center" }}>
-        <Panda />
-        <Typography variant="h5">Hello!</Typography>
-        <Typography variant="h5">I am Tiago Wu</Typography>
+      <Box display="flex" alignItems="center" justifyContent="center" flexDirection={{ xs: "column", sm: "row" }} height="100%">
+        <Box minWidth={{ xs: "180px", sm: "240px", xl: "360px" }} paddingRight={{ sm: "64px" }} paddingBottom={{ xs: "16px", sm: "0px" }}>
+          <Panda />
+        </Box>
+        <Box display="flex" alignItems="flex-start" justifyContent="center" flexDirection="column" maxWidth={{ xs: "600px", xl: "900px" }}>
+          <Typography sx={{ typography: { xs: "h4", sm: "h3" } }}>Hello!</Typography>
+          <Typography sx={{ typography: { xs: "h4", sm: "h3", xl: "h2" } }}>I am Tiago Wu.</Typography>
+          <Typography sx={{ typography: { xs: "h6", xl: "h4" } }}>
+            Third-year Computer Science student at Stony Brook University seeking a career in software development.
+          </Typography>
+        </Box>
       </Box>
     </Container>
   );
