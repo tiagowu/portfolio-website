91ac923aa3f3de4909aad621904e30ef616f0570
Tiago <tiagowu12@gmail.com>
2023-06-10 21:38:02 -0400

    Create icons data for project and skills section

---
+++ src/data/IconsData.js
@@ -0,0 +1,9 @@
+import { Icon } from "@iconify/react";
+
+export const iconsData = {
+  c: <Icon icon="devicon:c" />,
+  css: <Icon icon="logos:css-3" />,
+  html: <Icon icon="logos:html-5" />,
+  java: <Icon icon="logos:java" />,
+  javascript: <Icon icon="logos:javascript" />,
+};
