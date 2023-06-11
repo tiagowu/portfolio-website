d78617a8c0e459f6ae957327b8fa83da5ff7a068
Tiago <tiagowu12@gmail.com>
2023-06-10 21:41:17 -0400

    Import Logo SVG and replace placeholder with logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,15 @@
 import NavButtons from "./NavButtons";
+import { ReactComponent as Logo } from "../assets/Logo.svg";
 
 import { AppBar, Box, Toolbar } from "@mui/material";
 
 const NavBar = () => {
   return (
     <AppBar position="fixed">
-      <Toolbar disableGutters sx={{ height: "64px", paddingLeft: "16px", paddingRight: "16px" }}>
-        <Box flexGrow={1}>LOGO</Box>
+      <Toolbar disableGutters sx={{ height: "64px", pl: "16px", pr: "16px", backgroundColor: "#242933" }}>
+        <Box flexGrow={1}>
+          <Logo style={{ width: "80px" }} />
+        </Box>
         <NavButtons />
       </Toolbar>
     </AppBar>
