6561f8ca6bc8a4e62ef003aeae290f16e04c1abf
Tiago <tiagowu12@gmail.com>
2023-06-11 22:04:03 -0400

    Remove height attribute from container

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Container, Typography } from "@mui/material";
 
 const Footer = () => {
   return (
-    <Container id="footer" sx={{ textAlign: "center", height: "100px", pt: "36px" }}>
+    <Container id="footer" sx={{ textAlign: "center", pt: "36px" }}>
       <Typography variant="h6">©2023 Tiago Wu</Typography>
     </Container>
   );
