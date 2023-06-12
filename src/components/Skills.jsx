147eaa1ae56d70a61c5b0508527646791eba50f4
Tiago <tiagowu12@gmail.com>
2023-06-11 22:10:40 -0400

    Add grid layout and icons for skills data display

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,14 +1,32 @@
 import React from "react";
+import { iconsData } from "../data/IconsData";
+import { skillsData } from "../data/SkillsData";
 
-import { Container } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
 
 const Skills = () => {
   return (
-    <>
-      <Container sx={{ height: "100vh", paddingTop: "72px" }} id="skills">
+    <Container id="skills" sx={{ paddingTop: "72px" }}>
+      <Typography variant="h4" align="center" paddingBottom="32px">
         Skills
-      </Container>
-    </>
+      </Typography>
+      <Grid container spacing={2} justifyContent="center" alignItems="center">
+        {skillsData.map((section, sectionIndex) => (
+          <Grid item xs={12} key={sectionIndex}>
+            <Typography variant="h5" sx={{ display: "flex", justifyContent: "center", pb: "16px" }}>
+              {section.section}
+            </Typography>
+            <Grid container spacing={4} justifyContent="center" alignItems="center">
+              {section.skills.map((skill, skillIndex) => (
+                <Grid item key={skillIndex} sx={{ fontSize: "72px" }}>
+                  {iconsData[skill.toLowerCase()]}
+                </Grid>
+              ))}
+            </Grid>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 };
 
