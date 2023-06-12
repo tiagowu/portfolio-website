5ba32e59a5ece2d817363b95364d7482b9707c9f
Tiago <tiagowu12@gmail.com>
2023-06-11 22:13:33 -0400

    Create skills data for skills section

---
+++ src/data/SkillsData.js
@@ -0,0 +1,14 @@
+export const skillsData = [
+  {
+    section: "Programming Languages",
+    skills: ["C", "Java", "JavaScript", "Python"],
+  },
+  {
+    section: "Frontend Development",
+    skills: ["Bootstrap", "CSS", "HTML", "MaterialUI", "ReactJS"],
+  },
+  {
+    section: "Backend Development",
+    skills: ["Express", "MongoDB", "NodeJS"],
+  },
+];
