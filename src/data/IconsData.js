099489a8f7c12e9086d443e07479eb748b95a11f
Tiago <tiagowu12@gmail.com>
2023-06-11 22:11:50 -0400

    Change and add icon components

--- src/data/IconsData.js
+++ src/data/IconsData.js
@@ -1,9 +1,17 @@
 import { Icon } from "@iconify/react";
 
 export const iconsData = {
-  c: <Icon icon="devicon:c" />,
-  css: <Icon icon="logos:css-3" />,
-  html: <Icon icon="logos:html-5" />,
-  java: <Icon icon="logos:java" />,
-  javascript: <Icon icon="logos:javascript" />,
+  c: <Icon icon="skill-icons:c" />,
+  css: <Icon icon="skill-icons:css" />,
+  html: <Icon icon="skill-icons:html" />,
+  java: <Icon icon="skill-icons:java-light" />,
+  javascript: <Icon icon="skill-icons:javascript" />,
+  node: <Icon icon="skill-icons:nodejs" />,
+  mongodb: <Icon icon="skill-icons:mongodb" />,
+  bootstrap: <Icon icon="skill-icons:bootstrap" />,
+  materialui: <Icon icon="skill-icons:materialui-light" />,
+  reactjs: <Icon icon="skill-icons:react-light" />,
+  express: <Icon icon="skill-icons:expressjs-light" />,
+  nodejs: <Icon icon="skill-icons:nodejs-light" />,
+  python: <Icon icon="skill-icons:python-light" />,
 };
