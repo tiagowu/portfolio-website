87a2d6022ec610c388ba7ae299edc1c14ecdb742
Tiago <tiagowu12@gmail.com>
2023-06-13 15:18:37 -0400

    Refactor About component and improve code structure

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,19 @@
 import { aboutData } from "../data/AboutData";
+
 import { Box, Container, Typography } from "@mui/material";
 
 const About = () => {
+  const renderAboutText = () => {
+    return aboutData.map((text, index) => <Typography key={index}>{text}</Typography>);
+  };
+
   return (
     <Container id="about" sx={{ paddingTop: "72px" }}>
       <Typography variant="h4" align="center" pb="32px">
         About
       </Typography>
-      <Box sx={{ display: "flex", flexDirection: "column", margin: "auto", maxWidth: "720px", gap: "16px" }}>
-        {aboutData.map((text, index) => (
-          <Typography key={index}>{text}</Typography>
-        ))}
+      <Box display="flex" flexDirection="column" margin="auto" maxWidth="720px" gap="16px">
+        {renderAboutText()}
       </Box>
     </Container>
   );
