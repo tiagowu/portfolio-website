e5d6064db1b73d6e183397d25c669c3d8beb29cf
Tiago <tiagowu12@gmail.com>
2023-06-12 22:16:11 -0400

    Integrate EmailJS into contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,15 +1,40 @@
 import { useState } from "react";
-
 import { Box, Button, TextField, Typography } from "@mui/material";
+import emailjs from "emailjs-com";
+
+const SERVICE_ID = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+const TEMPLATE_ID = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+const USER_ID = process.env.REACT_APP_EMAILJS_USER_ID;
 
 const ContactForm = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [isEmailSent, setIsEmailSent] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(name, email, message);
+
+    emailjs
+      .send(
+        SERVICE_ID,
+        TEMPLATE_ID,
+        {
+          from_name: name,
+          from_email: email,
+          message: message,
+        },
+        USER_ID
+      )
+      .then(() => {
+        setName("");
+        setEmail("");
+        setMessage("");
+        setIsEmailSent(true);
+      })
+      .catch((error) => {
+        console.error("Error sending email:", error);
+      });
   };
 
   return (
@@ -28,6 +53,11 @@ const ContactForm = () => {
           onChange={(e) => setMessage(e.target.value)}
           required
         />
+        {isEmailSent && (
+          <Typography variant="body1" pb="8px">
+            Message successfully sent!
+          </Typography>
+        )}
         <Button variant="contained" color="primary" type="submit">
           Submit
         </Button>
