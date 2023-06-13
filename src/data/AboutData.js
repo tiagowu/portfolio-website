d5e488aa355f33f0c242a0f550a601c241e5fc9b
Tiago <tiagowu12@gmail.com>
2023-06-12 22:12:53 -0400

    Create about data for about section

---
+++ src/data/AboutData.js
@@ -0,0 +1,24 @@
+export const aboutData = [
+  `Hey there! I'm Tiago Wu, an ethusiastic Computer Science major driven 
+    by a passion for innovation. I find immense joy in unraveling complex 
+    problems and architecting efficient solutions that push the boundaries 
+    of what's possible. With hands-on experience across diverse domains, 
+    I bring a multifaceted perspective to every challenge. Whether it's 
+    developing robust software applications or optimizing algorithms, 
+    I'm always up for the exhilarating challenge of turning ideas into 
+    reality. From writing clean and elegant code to ensuring seamless user 
+    experiences, I take pride in consistently delivering high-quality work 
+    that not only meets but exceeds expectations.`,
+  `Beyond my professional pursuits, I draw insipiration from sports, 
+    particularly basketball, badminton, and table tennis. Engaging in these 
+    activities not only provides me with a well-rounded perspective but also 
+    enhances my strategic thinking and adaptability. My involvement in these 
+    hobbies has instilled in me the invaluable qualities of teamwork, 
+    discipline, and perseverance — attributes that  are equally 
+    valuable in the professional world.`,
+  `Thank you for visiting my portfolio website. I invite you to explore 
+    my work, browse through my achievements, and get a glimpse into the 
+    passion and dedication I bring to every project. If you have any inquiries, 
+    collaboration opportunities, or would simply like to connect, please feel 
+    free to reach out.`,
+];
