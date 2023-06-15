1ce4070438afd5198fd841c6cb57d5fcd5164f4c
Tiago <tiagowu12@gmail.com>
2023-06-14 22:00:58 -0400

    Refactor Skills component to use styled components

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,31 +2,56 @@ import { skillIcons } from "../data/IconsData";
 import { skillsData } from "../data/SkillsData";
 
 import { Container, Grid, Typography } from "@mui/material";
+import { styled } from "@mui/system";
 
 const Skills = () => {
   return (
-    <Container id="skills" sx={{ paddingTop: "72px" }}>
-      <Typography variant="h4" align="center" paddingBottom="32px">
-        Skills
-      </Typography>
-      <Grid container spacing={2} justifyContent="center" alignItems="center">
-        {skillsData.map((section, sectionIndex) => (
+    <Container id="skills" maxWidth={false}>
+      <Title variant="h4">Skills</Title>
+      <SectionGridContainer container>
+        {skillsData.map(({ section, skills }, sectionIndex) => (
           <Grid item xs={12} key={sectionIndex}>
-            <Typography variant="h5" sx={{ display: "flex", justifyContent: "center", pb: "16px" }}>
-              {section.section}
-            </Typography>
-            <Grid container spacing={4} justifyContent="center" alignItems="center">
-              {section.skills.map((skill, skillIndex) => (
-                <Grid item key={skillIndex} sx={{ fontSize: "72px" }}>
+            <Section variant="h5">{section}</Section>
+            <SkillGridContainer container>
+              {skills.map((skill, skillIndex) => (
+                <SkillGridItem item key={skillIndex}>
                   {skillIcons[skill.toLowerCase()]}
-                </Grid>
+                </SkillGridItem>
               ))}
-            </Grid>
+            </SkillGridContainer>
           </Grid>
         ))}
-      </Grid>
+      </SectionGridContainer>
     </Container>
   );
 };
+
+/* Styled Components */
+const Title = styled(Typography)(() => ({
+  textAlign: "center",
+  paddingBottom: 32,
+  paddingTop: 72,
+}));
+
+const SectionGridContainer = styled(Grid)(() => ({
+  alignItems: "center",
+  justifyContent: "center",
+  gap: 8,
+}));
+
+const Section = styled(Typography)(() => ({
+  textAlign: "center",
+  paddingBottom: 16,
+}));
+
+const SkillGridContainer = styled(Grid)(() => ({
+  alignItems: "center",
+  justifyContent: "center",
+  gap: 24,
+}));
+
+const SkillGridItem = styled(Grid)(() => ({
+  fontSize: 72,
+}));
 
 export default Skills;
