3c510761b33d59f0faea22bb582f36803a38cce0
Tiago <tiagowu12@gmail.com>
2023-06-14 22:01:31 -0400

    Refactor Projects component to use styled components

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,24 +2,34 @@ import ProjectCard from "./ProjectCard";
 import { projectsData } from "../data/ProjectsData";
 
 import { Container, Grid, Typography } from "@mui/material";
+import { styled } from "@mui/system";
 
 const Projects = () => {
   const reversedProjectsData = [...projectsData].reverse();
 
   return (
-    <Container id="projects" sx={{ pt: "72px" }}>
-      <Typography variant="h4" align="center" pb="32px">
-        Project
-      </Typography>
-      <Grid container spacing={4} justifyContent="center">
+    <Container id="projects" maxWidth={false}>
+      <Title variant="h4">Project</Title>
+      <ProjectGridContainer container spacing={4}>
         {reversedProjectsData.map((project, index) => (
-          <Grid item key={index} sx={{ alignItems: "center", justifyContent: "center" }}>
+          <Grid item key={index}>
             <ProjectCard project={project} />
           </Grid>
         ))}
-      </Grid>
+      </ProjectGridContainer>
     </Container>
   );
 };
+
+/* Styled Components */
+const Title = styled(Typography)(() => ({
+  textAlign: "center",
+  paddingBottom: 32,
+  paddingTop: 72,
+}));
+
+const ProjectGridContainer = styled(Grid)(() => ({
+  justifyContent: "center",
+}));
 
 export default Projects;
