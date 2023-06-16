289f873e0f2dc46df5d5870f39af9230f805a57e
Tiago <tiagowu12@gmail.com>
2023-06-15 22:07:34 -0400

    Refactor Header component to use styled components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,42 +1,91 @@
 import NavBar from "./NavBar";
 import LinkButton from "./LinkButton";
+import { infoIcons } from "../data/IconsData";
 import { ReactComponent as Panda } from "../assets/Panda.svg";
 
 import { Box, Container, Typography } from "@mui/material";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import ResumeIcon from "@mui/icons-material/Description";
+import { styled } from "@mui/system";
 
 const Header = () => {
+  const buttons = [
+    { link: "https://www.github.com/tiagowu", icon: infoIcons.github },
+    { link: "https://www.linkedin.com/in/tiago-wu/", icon: infoIcons.linkedin },
+    { link: "https://www.linkedin.com/in/tiago-wu/", icon: infoIcons.resume },
+  ];
+
   return (
-    <Container maxWidth={false} sx={{ display: "flex", alignItems: "center", minHeight: "100vh", pt: "72px", backgroundColor: "#232830" }}>
+    <HeaderContainer maxWidth={false}>
       <NavBar />
-      <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        flexDirection={{ xs: "column", sm: "row" }}
-        height="100%"
-        sx={{ maxWidth: { xs: "100%", sm: "900px" }, margin: "0 auto" }}
-      >
-        <Box minWidth={{ xs: "180px", sm: "240px" }} paddingRight={{ sm: "64px" }} paddingBottom={{ xs: "16px", sm: "0px" }}>
+      <ContentBox>
+        <LogoBox>
           <Panda />
-        </Box>
-        <Box display="flex" flexDirection="column">
-          <Typography sx={{ typography: { xs: "h4", sm: "h3" }, color: "white" }}>Hello!</Typography>
-          <Typography sx={{ typography: { xs: "h4", sm: "h3" }, color: "white" }}>I am Tiago Wu.</Typography>
-          <Typography sx={{ typography: { xs: "h6" }, color: "lightgray" }}>
-            Third-year Computer Science student at Stony Brook University seeking a career in software development.
+        </LogoBox>
+        <TextBox>
+          <Typography variant="h4" sx={{ typography: { sm: "h3" } }}>
+            Hello!
           </Typography>
-          <Box display="flex" justifyContent="center" alignItems="center" flexDirection="row" gap="12px">
-            <LinkButton link="https://www.github.com/tiagowu" icon={GitHubIcon} iconProps={{ fontSize: "32px" }} />
-            <LinkButton link="https://www.linkedin.com/in/tiago-wu/" icon={LinkedInIcon} iconProps={{ fontSize: "32px" }} />
-            <LinkButton link="https://www.linkedin.com/in/tiago-wu/" icon={ResumeIcon} iconProps={{ fontSize: "32px" }} />
-          </Box>
-        </Box>
-      </Box>
-    </Container>
+          <Typography variant="h4" sx={{ typography: { sm: "h3" } }}>
+            I am Tiago Wu.
+          </Typography>
+          <Subtitle variant="h6">Third-year Computer Science student at Stony Brook University seeking a career in software development.</Subtitle>
+          <ButtonContainer>
+            {buttons.map((button, index) => (
+              <LinkButton key={index} link={button.link} icon={button.icon} iconProps={{ fontSize: 32 }} />
+            ))}
+          </ButtonContainer>
+        </TextBox>
+      </ContentBox>
+    </HeaderContainer>
   );
 };
+
+const HeaderContainer = styled(Container)(() => ({
+  display: "flex",
+  alignItems: "center",
+  minHeight: "100vh",
+  paddingTop: 72,
+}));
+
+const ContentBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  flexDirection: "column",
+  height: "100%",
+  maxWidth: "100%",
+  margin: "0 auto",
+  [theme.breakpoints.up("sm")]: {
+    flexDirection: "row",
+    maxWidth: 900,
+  },
+}));
+
+const LogoBox = styled(Box)(({ theme }) => ({
+  minWidth: 180,
+  paddingBottom: 16,
+  [theme.breakpoints.up("sm")]: {
+    minWidth: 240,
+    paddingRight: 64,
+    paddingBottom: 0,
+  },
+}));
+
+const TextBox = styled(Box)(() => ({
+  display: "flex",
+  flexDirection: "column",
+}));
+
+const Subtitle = styled(Typography)(() => ({
+  color: "lightgray",
+}));
+
+const ButtonContainer = styled(Box)({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  flexDirection: "row",
+  gap: "12px",
+  height: "48px",
+});
 
 export default Header;
