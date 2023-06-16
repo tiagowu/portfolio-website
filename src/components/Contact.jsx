6924bbb7d7a71394539c5b2fb1f72f2e0bcd5973
Tiago <tiagowu12@gmail.com>
2023-06-16 11:30:22 -0400

    Refactor Contact component and update ContactBox rendering

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,38 +2,35 @@ import ContactBox from "./ContactBox";
 import ContactForm from "./ContactForm";
 import { infoIcons } from "../data/IconsData";
 
-import { Container, Box, Typography, useTheme } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
+
+const contactInfo = [
+  { label: "Email", content: "tiagowu12@gmail.com", icon: infoIcons.email },
+  { label: "Github", content: "https://www.github.com/tiagowu", icon: infoIcons.github },
+  { label: "LinkedIn", content: "https://www.linkedin.com/in/tiago-wu/", icon: infoIcons.linkedin },
+];
 
 const Contact = () => {
-  const theme = useTheme();
-
-  const renderContactBoxes = () => {
-    return (
-      <>
-        <ContactBox label="Email" content="tiagowu12@gmail.com" icon={() => infoIcons.email} />
-        <ContactBox label="Github" content="https://www.github.com/tiagowu" icon={() => infoIcons.github} />
-        <ContactBox label="LinkedIn" content="https://www.linkedin.com/in/tiago-wu/" icon={() => infoIcons.linkedin} />
-      </>
-    );
-  };
-
   return (
     <Container id="contact" maxWidth={false}>
       <Typography variant="h4" align="center" pt={9} pb={4}>
         Contact
       </Typography>
-      <Box margin="auto" maxWidth={1300}>
+      <Box margin="auto" maxWidth={1000}>
         <Box
           display="flex"
           flexDirection={{ xs: "column", sm: "row" }}
           justifyContent="center"
           p={4}
-          border={`1px solid ${theme.palette.primary.main}`}
-          borderRadius={1}
+          border={2}
+          borderRadius={4}
+          borderColor="primary.main"
         >
           <ContactForm />
           <Box display="flex" flexDirection="column" align="center" pl={{ sm: "16px" }} mt={6} gap={4}>
-            {renderContactBoxes()}
+            {contactInfo.map(({ label, content, icon }) => (
+              <ContactBox key={label} label={label} content={content} icon={icon} />
+            ))}
           </Box>
         </Box>
       </Box>
