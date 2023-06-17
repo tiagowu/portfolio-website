b7af063bdf833f6e21bbd21cc2bb0a45ada63aee
Tiago <tiagowu12@gmail.com>
2023-06-17 12:07:11 -0400

    Add navigation links using NavButtons component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,20 @@
+import NavButtons from "./NavButtons";
+
 import { Container, Typography } from "@mui/material";
 import { styled } from "@mui/system";
 
 const Footer = () => {
+  const navigations = [
+    { id: "about", label: "ABOUT" },
+    { id: "education", label: "EDUCATION" },
+    { id: "projects", label: "PROJECTS" },
+    { id: "skills", label: "SKILLS" },
+    { id: "contact", label: "CONTACT" },
+  ];
+
   return (
     <FooterContainer id="footer">
+      <NavButtons navigations={navigations} />
       <Typography variant="h6">©2023 Tiago Wu</Typography>
     </FooterContainer>
   );
