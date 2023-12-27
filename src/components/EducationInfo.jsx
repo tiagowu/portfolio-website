6f0cf44f53e80e67e107599c29f2b1ac812b92e6
Tiago <tiagowu12@gmail.com>
2023-12-27 13:39:30 -0500

    Update GPA in EducationInfo

--- src/components/EducationInfo.jsx
+++ src/components/EducationInfo.jsx
@@ -11,7 +11,7 @@ const EducationInfo = () => {
         <Typography variant="h6">Stony Brook University</Typography>
         <Typography variant="h6">Computer Science, B.S.</Typography>
         <Typography variant="h6">Exp. Grad: May 2024</Typography>
-        <Typography variant="h6">Cumulative GPA: 3.83</Typography>
+        <Typography variant="h6">Cumulative GPA: 3.8</Typography>
       </EducationTextBox>
     </EducationInfoBox>
   );
